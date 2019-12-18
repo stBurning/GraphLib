f43645d9357083acb7264400c22312f6b0e08595
Устинов Константин <richardocouls@gmail.com>
2019-12-18 15:04:46 +0300

    Add files via upload

--- GraphLib/GraphLib/Program.cs
+++ GraphLib/GraphLib/Program.cs
@@ -1,36 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using GraphLib;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GraphLib {
-    
+
     internal class Program {
-
-        public static void Main(string[] args) {
-
-            double[,] data = new double[6, 6];
-            for(int i = 0; i < data.GetLength(0); i++) {
-                for (int j = 0; j < data.GetLength(0); j++) {
-                    data[i, j] = 0;
+        public static Graph CreateGraph(int n) {
+            List<Vertex> Vertices = new List<Vertex>();
+            List<WeightedEdge> Edges = new List<WeightedEdge>();
+            Random random = new Random();
+            for (int i = 0; i < n; i++) {
+                Vertices.Add(new Vertex<int>(i));
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    Edges.Add(new WeightedEdge(Vertices[i], Vertices[j], Math.Round(random.NextDouble(), 3)));
                 }
             }
-            data[1, 0] = 4;
-            data[1, 2] = 7;
-            data[1, 3] = 1;
-            data[3, 0] = 2;
-            data[1, 5] = 1;
-            data[5, 0] = 1;
-            data[2, 4] = 3;
-            data[4, 1] = 1;
-            Graph graph = new Graph(data);
-            Console.WriteLine(graph.ToString());
-            List<int> way = graph.GetLowestWay(2, 3);
-            Console.WriteLine(way);
-            foreach (int i in way) {
-                Console.WriteLine(i);
+            return new Graph(Vertices, Edges);
+        }
+
+        public static void Main(string[] args){
+
+
+            
+            //Console.WriteLine(graph.ToString());
+            //List<Vertex> way = graph.GetLowestWay(2, 0);
+            //if (way.Count == 0) Console.Write("Нет необходимого пути ");
+            //for(int i = 0; i < way.Count; i++){
+              //  Console.Write(way[i]);
+               // if (i != way.Count - 1) Console.Write("->");
+            //}
+            for(int i = 100; i < 2000; i += 100) {
+                Graph graph = CreateGraph(i);
+                System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+                graph.GetLowestWay(i-1, 0);
+                timer.Stop();
+                TimeSpan ts = timer.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+                Console.WriteLine("Count of vertices: "+ i + ": "+"Time:" + elapsedTime);
             }
 
-
+            Console.ReadKey();
         }
     }
 }
--- GraphLib/GraphLib/Properties/AssemblyInfo.cs
+++ GraphLib/GraphLib/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
 [assembly: AssemblyTitle("GraphLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,23 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4575DDC6-6625-468B-8FCD-2B6932BBC517")]
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("fd2ba757-ef24-4e65-be9b-7cd7a217d8c5")]
 
-// Version information for an assembly consists of the following four values:
+// Сведения о версии сборки состоят из следующих четырех значений:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
